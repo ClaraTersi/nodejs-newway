459ff677637eb166a89a7405d0bd0564a205a6d8
Clara Tersi <claratersi@Claras-MacBook-Pro.local>
2019-03-11 22:21:19 -0300

    correção

--- exemplos/src/metodos-array/filter/usandoFilter.js
+++ exemplos/src/metodos-array/filter/usandoFilter.js
@@ -4,7 +4,7 @@
 const arrayOriginal = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const arrayDePares = arrayOriginal.filter((elem) => {
-  if (elem % 2 === 0) return elem;
+  return elem % 2 === 0;
 });
 
 console.log(`Array original: ${arrayOriginal}`);
